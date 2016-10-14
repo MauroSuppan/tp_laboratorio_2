7cf07138b9ac9c0df547eb3efd7acc182d196ede
MauroSuppan <mauro.s@outlook.com>
2016-10-14 00:27:13 -0300

    Ejercicio Calculadora
    Ejercicio calculadora Windows Forms

---
+++ Clase5TP/Clase5TP/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Clase5TP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+           
+            InitializeComponent();
+        }
+        
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            //se crean 2 objetos de tipo numero
+            Numero num1 = new Numero(this.txtNumero1.Text.ToString());
+            Numero num2 = new Numero(this.txtNumero2.Text.ToString());
+
+            this.lblResultado.Visible = true;
+            //realiza el metodo operar y lo que retorna lo conviente en string y lo guarda en lblresultado            
+            this.lblResultado.Text = (Calculadora.Operar(num1, num2, cmbOperacion.Text)).ToString(); 
+            
+        }
+
+        //el boton llama al metodo Limpiar
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            this.Limpiar();
+        }
+        //el metodo borra el contenido de los textbox, carga "" en combobox, y le saca la visibilidad a lblResultado
+        private void Limpiar()
+        {
+            this.txtNumero1.Clear();
+            this.txtNumero2.Clear();
+            this.cmbOperacion.Text = "";
+            this.lblResultado.Visible = false;
+
+        }
+
+        private void txtNumero1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
---
+++ Clase5TP/Clase5TP/Numero.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Clase5TP
+{
+    class Numero
+    {
+        private double numero;
+
+      
+        
+        public double getNumero()
+        {
+            return this.numero;
+        }
+
+        /// <summary>
+        /// llama al constructor que recibe un double e inicializa a numero en 0
+        /// </summary>
+        public Numero() :this((double) 0)
+        {
+           
+        }
+        /// <summary>
+        /// recibe un double y se lo asigna numero
+        /// </summary>
+        /// <param name="numero">Valor a asignar</param>
+        public Numero(double numero)
+        {
+            this.numero = numero;  
+        }
+        /// <summary>
+        /// recibe un string que se va a validar y asignar
+        /// </summary>
+        /// <param name="numero">numero a validar</param>
+        public Numero(string numero)
+        {
+           
+            this.setNumero(numero);
+        }
+        /// <summary>
+        /// valida y setea el valor
+        /// </summary>
+        /// <param name="numero">string a validar</param>
+        private void setNumero(string numero)
+        {
+           
+
+            this.numero = Numero.ValidarNumero(numero);
+            
+
+        }
+
+        /// <summary>
+        /// convierte un string a double, en caso de no poder retorna 0
+        /// </summary>
+        /// <param name="numeroString">string a convertir</param>
+        /// <returns>retorna un double</returns>
+        private static double ValidarNumero(string numeroString)
+        {
+         
+            double numeroDouble;
+            if (double.TryParse(numeroString, out numeroDouble))
+                return numeroDouble;
+            return 0;
+        }
+
+        //sobrecarga del operador +
+        public static double operator +(Numero num1, Numero num2)
+        {
+            double retValue;
+            retValue = num1.numero + num2.numero;
+            return retValue;
+
+        }
+        //sobrecarga del operador -
+        public static double operator -(Numero num1, Numero num2)
+        {
+            double retValue;
+            retValue = num1.numero - num2.numero;
+            return retValue;
+
+        }
+        //sobrecarga del operador*
+        public static double operator *(Numero num1, Numero num2)
+        {
+            double retValue;
+            retValue = num1.numero * num2.numero;
+            return retValue;
+
+        }
+        //sobrecarga del operador /
+        public static double operator /(Numero num1, Numero num2)
+        {
+            double retValue;
+            retValue = num1.numero / num2.numero;
+            return retValue;
+
+        }
+
+
+    }
+}
